e2111db72b7d3fe5fbe0ed0841ed6c955d5961ec
SinnerA <wu181184@sina.com>
2015-07-11 11:35:54 +0800

    添加测试函数

---
+++ TinySTL/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <memory>
+
+#include "Alloc.h"
+#include "Construct.h"
+
+using namespace std;
+
+int main(){
+	cout << "Helo World" << endl;
+
+	getchar();
+	return 0;
+}
