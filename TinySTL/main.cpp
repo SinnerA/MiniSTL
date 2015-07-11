bceea20037ca8bd2fada286db7beaba1ff0e2c08
SinnerA <wu181184@sina.com>
2015-07-11 12:15:59 +0800

    测试空间配置器

--- TinySTL/main.cpp
+++ TinySTL/main.cpp
@@ -9,6 +9,10 @@ using namespace std;
 int main(){
 	cout << "Helo World" << endl;
 
+	for(int i = 1; i <= 100000; ++i){
+		TinySTL::alloc::allocate(i % 128 * sizeof(int));
+	}
+
 	getchar();
 	return 0;
 }
